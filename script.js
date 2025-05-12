d822f96427187e581ea0c0ace05f8700a98cb5f9
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-05-12 15:26:30 +0200

    feat: snack 2

--- script.js
+++ script.js
@@ -21,3 +21,27 @@ function getPost(id) {
 
 getPostTitle(1).then(title => console.log(title));
 getPost(4).then(post => console.log(post));
+
+
+function lanciaDado() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const probabilitaIncastro = Math.random();
+            if (probabilitaIncastro < 0.2) {
+                reject("Il dado si è incastrato!");
+            } else {
+                const numero = Math.floor(Math.random() * 6) + 1;
+                resolve(numero);
+            }
+        }, 3000);
+    });
+}
+
+
+lanciaDado()
+    .then(numero => {
+        console.log("È uscito il numero:", numero);
+    })
+    .catch(errore => {
+        console.error("Errore:", errore);
+    });
